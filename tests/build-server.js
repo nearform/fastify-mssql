f5522a1493e5719eff0a0561d080a4c45cf6a36d
gilach <grzegorz@ilach.pl>
2021-05-25 12:03:11 +0200

    fix: accept additional config options for mssql connection
    * added eslint configuration & CI step for linting
    * added docker-compose file for local development
    * fixed test setup/teardown
    * bumped mssql version to 7.1.0
    * fixed mssql config so that additional options will carry over

--- tests/build-server.js
+++ tests/build-server.js
@@ -2,7 +2,7 @@ const fastify = require('fastify')
 
 function build(opts = {}) {
   const app = fastify(opts)
-  app.get('/', async function (request, reply) {
+  app.get('/', async function () {
     return { hello: 'world' }
   })
 
--- tests/plugin.test.js
+++ tests/plugin.test.js
@@ -1,63 +1,66 @@
 const buildServer = require('./build-server')
 
-const plugin = require('../plugin.js');
-const MSSql = require('mssql');
+const plugin = require('../plugin.js')
 
 const { getPool } = require('./utils')
 
+describe('fastify-mssql', () => {
+  let app
 
-let app;
-beforeEach(async () => {
-  const pool = await getPool();
-  try {
+  beforeAll(async () => {
+    const pool = await getPool()
     app = buildServer()
-    await pool.query(`CREATE DATABASE TestSuite`);
-    await pool.query(`USE TestSuite`);
-    await pool.query("CREATE TABLE [dbo].[Users] ([id] bigint,[name] varchar(25), [email] varchar(25) PRIMARY KEY (id));");
-    await pool.query("INSERT INTO [dbo].[Users] ([id], [name], [email]) VALUES ('1', N'Foobar', N'foobar@gmail.com');");
-    await pool.query("INSERT INTO [dbo].[Users] ([id], [name], [email]) VALUES ('2', N'fizzbuzz', N'fizzbuzz@gmail.com');");
-  } catch (err) {
-    if (err.message.match(`Database 'TestSuite' already exists`)) {
-      // do nothing, it's fine
-    } else {
-      throw err;
-    }
-  }
-
-
-})
-afterEach(async () => {
-  const pool = await getPool();
-  await pool.query(`USE TestSuite`);
-  await pool.query("DROP TABLE [dbo].[Users]");
-  await pool.close();
-  app.close();
-})
-
-test('MSSQL plugin is loaded', async () => {
-
-  app.register(plugin, {
-    user: 'sa',
-    password: 'S3cretP4ssw0rd!',
-    database: 'TestSuite'
+    await pool.query(`
+      IF NOT EXISTS (SELECT 1 FROM sys.databases WHERE name = 'TestSuite')
+        CREATE DATABASE TestSuite;
+    `)
+    await pool.query(`USE TestSuite`)
+    await pool.query(
+      'CREATE TABLE [dbo].[Users] ([id] bigint,[name] varchar(25), [email] varchar(25) PRIMARY KEY (id));'
+    )
+    await pool.query(
+      "INSERT INTO [dbo].[Users] ([id], [name], [email]) VALUES ('1', N'Foobar', N'foobar@gmail.com');"
+    )
+    await pool.query(
+      "INSERT INTO [dbo].[Users] ([id], [name], [email]) VALUES ('2', N'fizzbuzz', N'fizzbuzz@gmail.com');"
+    )
   })
 
-  app.get('/users', async function (req, reply) {
-    try {
-      await app.mssql.pool.connect();
-      const res = await app.mssql.pool.query('SELECT * FROM [dbo].[Users];');
-      return { users: res.recordset }
-    } catch (err) {
-      return { error: err.message }
-    }
-  })
-  const response = await app.inject({
-    method: 'GET',
-    url: '/users'
+  afterAll(async () => {
+    const pool = await getPool()
+    await pool.query(`USE TestSuite`)
+    await pool.query('DROP TABLE IF EXISTS [dbo].[Users]')
+    await pool.close()
+    app.close()
   })
 
-  const body = JSON.parse(response.body);
-  expect(app.mssql.pool).not.toBe(undefined);
-  expect(response.statusCode).toBe(200);
-  expect(body.users.length).toBe(2);
+  test('MSSQL plugin is loaded', async () => {
+    app.register(plugin, {
+      user: 'sa',
+      password: 'S3cretP4ssw0rd!',
+      database: 'TestSuite',
+      options: {
+        trustServerCertificate: true
+      }
+    })
+
+    app.get('/users', async function () {
+      try {
+        await app.mssql.pool.connect()
+        const res = await app.mssql.pool.query('SELECT * FROM [dbo].[Users];')
+        return { users: res.recordset }
+      } catch (err) {
+        return { error: err.message }
+      }
+    })
+    const response = await app.inject({
+      method: 'GET',
+      url: '/users'
+    })
+
+    const body = JSON.parse(response.body)
+    expect(app.mssql.pool).not.toBe(undefined)
+    expect(response.statusCode).toBe(200)
+    expect(body.users.length).toBe(2)
+  })
 })
